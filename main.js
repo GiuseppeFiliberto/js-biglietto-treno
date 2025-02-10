403234b0769464e76278cc526e7607b041f77f53
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-10 17:17:26 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const questionKm = Number (prompt('Quanti chilometri ha bisogno di percorerre?'))
+console.log(questionKm);
+
+const questionAge = Number (prompt ('Scrivere la propria età in numeri [ es 30]'))
+console.log(questionAge);
