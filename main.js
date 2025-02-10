71135cab45223dbc782a876dfbf81a006e0140e4
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-10 17:33:09 +0100

    add price variables

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ console.log(questionKm);
 
 const questionAge = Number (prompt ('Scrivere la propria età in numeri [ es 30]'))
 console.log(questionAge);
+
+let standardPrice = questionKm * 0.21;
+
+
+if (questionAge < 18){
+    let discount20 = standardPrice * 20 / 100;
+} else if (questionAge > 65) {
+    let discount40 = standardPrice * 40 / 100;
+}
