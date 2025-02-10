02d5acd8492e59dc45cd936a89c1399d5797c31e
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-02-10 18:12:54 +0100

    fix alert string + fix discount variables

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const questionAge = Number (prompt ('Scrivere la propria età in numeri [ es 30]
 console.log(questionAge);
 
 const standardPrice = questionKm * 0.21;
-const discount20 = standardPrice * 20 / 100;
-const discount40 = standardPrice * 40 / 100;
+const discount20 = standardPrice * 0.20;
+const discount40 = standardPrice * 0.40;
 
 
 if (questionAge < 18){
-    alert ('Il costo del tuo biglietto è €' `${discount20}` )
+    alert (`Il costo del tuo biglietto è € ${discount20.toFixed(2)} - hai diritto ad un 20% di sconto` );
 } else if (questionAge > 65) {
-    alert ('Il costo del tuo biglietto è €' `${discount40}`)
+    alert  (`Il costo del tuo biglietto è € ${discount40.toFixed(2)} - hai diritto ad un 40% di sconto`);
 } else {
-    alert ('Il costo del tuo biglietto è €' `${standardPrice}`)
+    alert (`Il costo del tuo biglietto è € ${standardPrice.toFixed(2)}`);
 }
